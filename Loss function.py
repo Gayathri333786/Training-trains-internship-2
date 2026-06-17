844cd653ecabbc1fab0a4227f554de7cd249e5e3
Gayathri M <gayathri3333u@gmail.com>
2026-06-17 14:20:01 +0530

    Add loss function example in Loss function.py

---
+++ Loss function.py
@@ -0,0 +1,10 @@
+# Loss Function Example
+
+actual_marks = 80
+predicted_marks = 70
+
+loss = abs(actual_marks - predicted_marks)
+
+print("Actual Marks:", actual_marks)
+print("Predicted Marks:", predicted_marks)
+print("Loss:", loss)
