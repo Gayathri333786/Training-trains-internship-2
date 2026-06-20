dddc80dc581f202e39e3c7dfa9c2acb954d9242c
Gayathri M <gayathri3333u@gmail.com>
2026-06-20 15:24:04 +0530

    Add PCA implementation for Iris dataset visualization

---
+++ Train/PCA.py
@@ -0,0 +1,23 @@
+from sklearn.datasets import load_iris
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+pca = PCA(n_components=2)
+
+X_pca = pca.fit_transform(X)
+
+plt.scatter(
+    X_pca[:,0],
+    X_pca[:,1],
+    c=y
+)
+
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+
+plt.show()
